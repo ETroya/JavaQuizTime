c724052bc0b0f5afde482cd9ddd18833ce2ddeda
Edwin Troya <edtroyajr@gmail.com>
2020-12-08 03:42:22 -0800

    missed work

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # JavaQuizTime
 Have what it takes to understanding JavaScript... Take the Quiz and find out!
+<img style="-webkit-user-select: none;margin: auto;" src="https://static.wikia.nocookie.net/digimon/images/1/15/DotAgumon_vg.gif/revision/latest?cb=20081027135117">
+
+By clicking Bring the Heat it should take you to your exam. 
+If you would like yo check out your high score its righ below. 
+
+<img src="where_to_click.png" alt="menu">
--- script.js
+++ script.js
@@ -1,68 +1,91 @@
-// 
-const question = document.querySelector("#question");
-const question = Array.from(document.querySelectorAll("choice-text"));
-const progressText = document.querySelector("#progressText");
-const scoreText = document.querySelector("#score");
-const progressBarFull = document.querySelector("#progressBarFull");
-
-let currentQuestions = {};
-let acceptingAnswers = true;
-let score = 0 ;
-let questionCounter =0;
-let availableQuestions=[];
+var name;
+var names =[];
+var highScores=[];
+var timer;
+var seconds;
+var answerTimer; 
+var answerSeconds =1;
+var score;
+var finalScore;
+var questions;
+var wrongAnswers;
+var rightAnswer;
+var question = document.querySelector (".question");
+var rightAnswer =document.querySelector(".rightAnswer");
+var wrongAnswers =document.querySelector(".wrongAnswer");
+var time = document.querySelector(".timer");
+var start = document.querySelector("#startButton");
 
 //Questions for the exam//
-
 let questions = [
     {
     question:"Inside which HTML element do we put the JavaScript?",
-    rightanswer:"<script>",
-    wronganswers: ["<scripting.index>","<java>","<Java.index>","None of the above <bootstrap></bootstrap>"],   
+    rightAnswer:"<script>",
+    wrongAnswers:["java", "Java.index", "scripting.index"],
+
 },
 {
     question:"Which of the following would not create a varible",
-    rightanswer: "function"
-    wronganswers: ["const","let","var"],
+    rightAnswer: "function",
+    wrongAnswers: ["const","let","var"],
 },
 {
     question:"Where is the correct place to insert a JavaScript?",
-    rightanswer: "Both the <head> section and the <body> section are correct", 
-    wronganswers: ["The <body> section","The <head> section"],
+    rightAnswer: "Both the <head> section and the <body> section are correct", 
+    wrongAnswers: ["The <body> section","The <head> section"],
 },
 {
-    question:"What is the correct syntax for referring to an external script called "xxx.js"?",
-    rightanswer:"<script name=”xxx.js”>", 
-    wronganswers: ["<script href=”xxx.js”>","<script href=”xxx.js”>"],
+    question:"What is the correct syntax for referring to an external script called xxx.js?",
+    rightAnswer:"<script name=”xxx.js”>", 
+    wrongAnswers: ["<script href=”xxx.js”>","<script href=”xxx.js”>"],
 },
 {
     question:"The external JavaScript file must contain the <script> tag",
-    rightanswer:"False",
-    wronganswers: "True",
+    rightAnswer:"False",
+    wrongAnswers: "True",
 },
 {
-    question:"How do you write "HelloWorld" in an alert box?",
-    rightanswer:"alert(Hello World”)"
-    wronganswers:["msgBox(“Hello World”);", "msg Box((Hello world”);","alertBox(“Hello World);"],
+    question:"How do you write HelloWorld in an alert box?",
+    rightAnswer:"alert(Hello World”)",
+    wrongAnswers:["msgBox(“Hello World”);", "msg Box((Hello world”);","alertBox(“Hello World);"],
 },
 {
     question:"How do you create a function in JavaScript?",
-    rightanswer:"Function myFunction ()",
-    wronganswers: ["Function:myFuncation()","Function= myFunction()"],
+    rightAnswer:"Function myFunction ()",
+    wrongAnswers: ["Function:myFuncation()","Function= myFunction()"],
 },
 {
-    question:
-    rightanswer:
-    wronganswers: 
+    question:"How do you call a function named: myFunction?",
+    rightAnswer: "myFunction()",
+    wrongAnswers: "call myFunction", "call function myFunction()"
 },
 {
-    question:
-    rightanswer:
-    wronganswers: 
+    question:"What is the correct way to write a JavaScript array?",
+    rightAnswer:"var colors = ["red","green", "blue"],
+    wrongAnswers: "var colors = ""red", "green", "blue" ", "var colors = 1 = ("red"), 2 = ("green"), 3 = ("blue")" ,"var colors = (1:"red", 2:"green", 3:"blue")",
 },
 {
-    question:
-    rightanswer:
-    wronganswers: 
+    question:"Which event occurs when the user clicks on an HTML element?",
+    rightAnswer: "onclick",
+    wrongAnswers: "onmouseover", "onmouseclick", "onchange",
 },
-
 ]
+
+// Adding Time//
+rightAnswer.addEventListener ("click" ,function(){
+answer.textContent = "You got this!"
+seconds= seconds + 10;
+score = score + 10;
+questions();
+}
+
+// Decrease in time
+wrongAnswers.addEventListener ("click", function(){
+answer.textContent ="I dont think that was right"
+seconds= seconds -10;
+question();
+}
+
+function startQuiz() {
+    start.addEventListener("click", function(){
+        questions();
